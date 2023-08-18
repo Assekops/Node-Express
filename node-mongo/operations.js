0f49de2aff94cd2f3e37f3a8212baab033e9b713
Assefa Tadesse <assekopsynwa@gmail.com>
2023-08-18 12:02:07 -0500

    Node MongoDB Example 2

--- node-mongo/operations.js
+++ node-mongo/operations.js
@@ -1,33 +1,31 @@
-const Collection = require('mongodb/lib/collection');
-
 const assert = require('assert').strict;
 
-exports.inserDocument = (db, document, Collection, callback) => {
-    const coll = db.collection(Collection);
+exports.insertDocument = (db, document, collection, callback) => {
+    const coll = db.collection(collection);
     coll.insertOne(document, (err, result) => {
         assert.strictEqual(err, null);
         callback(result);
     });
 };
 
-exports.findDocument = (db, Collection, callback) => {
-    const coll = db.collection(Collection);
+exports.findDocuments = (db, collection, callback) => {
+    const coll = db.collection(collection);
     coll.find().toArray((err, docs) => {
         assert.strictEqual(err, null);
         callback(docs);
     });
 };
 
-exports.removeDocument = (db, document, Collection, callback) => {
-    const coll = db.collection(Collection);
+exports.removeDocument = (db, document, collection, callback) => {
+    const coll = db.collection(collection);
     coll.deleteOne(document, (err, result) => {
         assert.strictEqual(err, null);
         callback(result);
     });
 };
 
-exports.updateDocument = (db, document, update, Collection, callback) => {
-    const coll = db.collection(Collection);
+exports.updateDocument = (db, document, update, collection, callback) => {
+    const coll = db.collection(collection);
     coll.updateOne(document, { $set: update }, null, (err, result) => {
         assert.strictEqual(err, null);
         callback(result);
