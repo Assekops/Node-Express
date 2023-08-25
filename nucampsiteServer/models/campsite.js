da966fcdbb2b2a23394ecde504a16b8dc7f919af
Assefa Tadesse <assekopsynwa@gmail.com>
2023-08-25 18:02:35 -0500

    Mongoose Population

--- nucampsiteServer/models/campsite.js
+++ nucampsiteServer/models/campsite.js
@@ -16,8 +16,8 @@ const commentSchema = new Schema({
         required: true
     },
     author: {
-        type: String,
-        required: true
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
     }
 }, {
     timestamps: true
@@ -51,10 +51,9 @@ const campsiteSchema = new Schema({
         default: false
     },
     comments: [commentSchema]
-},
-    {
-        timestamps: true
-    });
+}, {
+    timestamps: true
+});
 
 const Campsite = mongoose.model('Campsite', campsiteSchema)
 
