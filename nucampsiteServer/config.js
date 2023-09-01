78e208950c99537cbad098eea45ef188bbcf174b
Assefa Tadesse <assekopsynwa@gmail.com>
2023-09-01 14:00:27 -0500

    Oauth

--- nucampsiteServer/config.js
+++ nucampsiteServer/config.js
@@ -1,4 +1,13 @@
+// module.exports = {
+//     'secretKey': '12345-67890-09876-54321',
+//     'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+// }
+
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '283062451129860',
+        clientSecret: '9e12f616d66e8945bf96bf6664ebad75'
+    }
 }
--- nucampsiteServer/models/user.js
+++ nucampsiteServer/models/user.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 
 
 const userSchema = new Schema({
+    facebookId: String,
     firstname: {
         type: String,
         default: ''
