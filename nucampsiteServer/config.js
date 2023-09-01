c6e05edeca46c3ef4ab88fefc7e2e5a50edef995
Assefa Tadesse <assekopsynwa@gmail.com>
2023-09-01 14:08:31 -0500

    Revert "Oauth"
    This reverts commit 78e208950c99537cbad098eea45ef188bbcf174b.

--- nucampsiteServer/config.js
+++ nucampsiteServer/config.js
@@ -1,13 +1,4 @@
-// module.exports = {
-//     'secretKey': '12345-67890-09876-54321',
-//     'mongoUrl': 'mongodb://localhost:27017/nucampsite'
-// }
-
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '283062451129860',
-        clientSecret: '9e12f616d66e8945bf96bf6664ebad75'
-    }
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
 }
--- nucampsiteServer/models/user.js
+++ nucampsiteServer/models/user.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 
 
 const userSchema = new Schema({
-    facebookId: String,
     firstname: {
         type: String,
         default: ''
